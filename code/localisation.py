7f77c5f3788bd95e1393cb96c56c993af60b53b4
HideBa <baba.papa1120.ba@gmail.com>
2024-10-09 20:57:24 +0200

    fix missing library

--- code/localisation.py
+++ code/localisation.py
@@ -60,6 +60,7 @@ def scatterplot(
     plt.show()
 
 
+# TODO: implement this function
 # Function to compute a central point (localization)
 def localisation(
     points: list[tuple[float, float, float]]
@@ -155,8 +156,8 @@ if __name__ == "__main__":
     ]
 
     wall_coordinates = pd.read_csv(WALL_COORDINATES_PATH)
-
+    points = [coord[:2] for coord in localised_coordinates]
+    print("lenght------", len(points))
     scatterplot(
-        [tuple(x) for x in wall_coordinates[["x", "y"]].values],
-        [coord[:2] for coord in localised_coordinates],
+        [tuple(x) for x in wall_coordinates[["x", "y"]].values], points
     )
