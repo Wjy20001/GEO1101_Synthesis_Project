e1a13550a44aea1342bdcfcf66e4aaf372c6b102
Noah <noahalting@gmail.com>
2024-10-23 11:54:01 +0200

    merged coord conversion, room validation and point_in_polygon scripts succesfully

--- code/point_in_polygon.py
+++ code/point_in_polygon.py
@@ -1,17 +1,19 @@
 import json
 from shapely.geometry import shape, Point
 import os
+import convertcoordinate as cc
 
-def point_in_polygon(x, y, geojson_file):
+def point_in_polygon(point: tuple[float, float], geojson_file_path:str):
+    x, y = point
     # Validate input types
     if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
         raise ValueError("Coordinates x and y must be numeric.")
     
-    if not os.path.exists(geojson_file):
-        raise FileNotFoundError(f"GeoJSON file '{geojson_file}' not found.")
+    if not os.path.exists(geojson_file_path):
+        raise FileNotFoundError(f"GeoJSON file '{geojson_file_path}' not found.")
     
     try:
-        with open(geojson_file) as f:
+        with open(geojson_file_path) as f:
             data = json.load(f)
     except json.JSONDecodeError as e:
         raise ValueError(f"Invalid GeoJSON file format: {e}")
@@ -39,14 +41,22 @@ def point_in_polygon(x, y, geojson_file):
 
 if __name__ == "__main__":
     # Example usage
-    x, y = 5.0, 4.0
-    geojson_file = os.path.join('data', 'room_validation', 'test_floorplan.geojson')
+    #coordinate of BK in EPSG:28992 for testing purposes
+    test_eastwing = (85224.66,446928.80)
+    test_westwing = (85138.73,446829.61)
+    test_central = (85207.94,446863.64)
+
+    test_coord = test_westwing
+
+    #convert to latlong
+    test_coord = cc.convert_coordinates(test_coord)
+    geojson_file = os.path.join('data', 'room_validation', 'test_floorplan_latlong.geojson')
 
     try:
-        resulting_room = point_in_polygon(x, y, geojson_file)
+        resulting_room = point_in_polygon(test_coord, geojson_file)
         if resulting_room:
-            print(f"The point ({x}, {y}) is inside {resulting_room}.")
+            print(f"The point {tuple(test_coord)} is inside {resulting_room}.")
         else:
-            print(f"The point ({x}, {y}) is outside any of the polygons in {geojson_file}.")
+            print(f"The point {tuple(test_coord)} is outside any of the polygons in {geojson_file}.")
     except (FileNotFoundError, ValueError) as e:
         print(f"Error: {e}")
