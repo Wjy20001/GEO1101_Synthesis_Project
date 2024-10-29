705773a989681cb0ed6c46023d3fb9558dd62b7b
Noah <noahalting@gmail.com>
2024-10-29 10:12:57 +0100

    added more validation data and changed script, currently we are at 93.97%!

--- code/room_validation.py
+++ code/room_validation.py
@@ -55,7 +55,15 @@ def print_statistics(df: pd.DataFrame, toggle: bool) -> None:
     print("=" * 40)
     print(stats_report)
     print("=" * 40)
-    
+
+    # Filter for mismatches and print details if any
+    mismatches = df[~validate]
+    if not mismatches.empty:
+        print("Mismatched Entries:")
+        for _, row in mismatches.iterrows():
+            print(f"Position ID: {row['position_id']}\tTrue Room: {row['true_room']}\tFound Room: {row['found_room']}\timage_names: {row['user_image_name']}")
+    else:
+        print("All entries matched correctly.")
 
 if __name__ == "__main__":
     #define paths
@@ -73,13 +81,15 @@ if __name__ == "__main__":
     # get validation data as dataframe
     df_full: pd.DataFrame = pd.read_csv(csv_path_validation, dtype=pd.StringDtype())
 
+    df_full['position_id'] = df_full['position_id'].astype(int)
 
     # single image matching = True
     # multiple image matching = False
     diagnostics_toggle = False
 
-    N_best_matches: int = 6 #top N best matches to use in DBSCAN
-    min_DBSCAN_samples: int = 3
+    # (DBSCAN) parameters
+    N_best_matches: int = 6 #default=6 
+    min_DBSCAN_samples: int = 2 #default=3
 
 
     if diagnostics_toggle:
@@ -90,13 +100,20 @@ if __name__ == "__main__":
         print_statistics(df_full, toggle=diagnostics_toggle)
     
     else:
-        #below we group by room so we can input 3 or more images for matching results
-        grouped_df = df_full.groupby('true_room')['user_image_name'].apply(list).reset_index()
+        #below we group by room so we can input 2 or more images for matching results
+
+        #group by position id to get image set for every position
+        grouped_df = df_full.groupby('position_id').agg({'user_image_name': list,     #list the images for a single user position      
+                                                    'true_room': 'first' #because it will be equal for every image with same position_id
+                                                    }).reset_index()
+
+        #filter out positions that have one single image (just to be sure!)
         filtered_df = grouped_df[grouped_df['user_image_name'].apply(len) >= 2].copy()
 
         filtered_df['found_room'] = filtered_df['user_image_name'].apply(lambda x: get_room_name(x, rooms_json_path, top_n_matches=N_best_matches, 
                                                                                                  min_DBSCAN_samples=min_DBSCAN_samples))
         filtered_df['found_room'] = filtered_df['found_room'].astype(pd.StringDtype())
 
+        
         print_statistics(filtered_df, toggle=diagnostics_toggle)
 
