051cd67630c05ef8f26f5742ecfb3d43dd06e8ab
HideBa <baba.papa1120.ba@gmail.com>
2024-10-30 15:54:16 +0100

    update cors settings

--- API/main.py
+++ API/main.py
@@ -32,7 +32,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
     allow_origin_regex="https://.*\.netlify\.app",
 )
@@ -202,7 +202,7 @@ async def find_route(start_room_name: str, end_room_name: str):
             floorplan_json_path,
             nodes_json_path,
             route_json_path,
-            restricted_rooms=rooms_to_exclude
+            restricted_rooms=rooms_to_exclude,
         )
 
     except subprocess.CalledProcessError as e:
